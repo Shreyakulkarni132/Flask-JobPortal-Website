19498ba7c389f48a7a5ce68dbea247a911c80af5
Shreya Kulkarni <shreya.kulkarni132@gmail.com>
2025-03-21 17:51:18 +0530

    first

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Scientist',
+        'location': 'Remote',
+        'salary': '$120,000'
+    },
+    {
+        'id': 2,
+        'title': 'FrontEnd Developer',
+        'location': 'Remote',
+        'salary': '$220,000'
+    },
+    {
+        'id': 3,
+        'title': 'Data Analyst',
+        'location': 'Remote',
+        'salary': '$90,000'
+    },
+    {
+        'id': 4,
+        'title': 'SDE',
+        'location': 'Remote',
+        'salary': '$110,000'
+    }
+
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs(): 
+    return jsonify(JOBS)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
